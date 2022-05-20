7f430122b0a55e63a78d37483c39040ec985f567
Alex Santos <alexdsantosv@gmail.com>
2022-05-20 02:56:51 -0600

    [ Add ] show more button

--- src/pages/home/components/list/index.tsx
+++ src/pages/home/components/list/index.tsx
@@ -1,6 +1,8 @@
-import Icon from 'components/icon'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+// COMPONENTS
+import Icon from 'components/icon'
 
 // HOOKS
 import useDogsList from './hooks'
@@ -9,6 +11,12 @@ import useDogsList from './hooks'
 import Styles from './style.module.scss'
 
 const DogList: React.FC = () => {
+	// MORE
+	const [showMore, setShowMore] = useState<boolean>(false)
+
+	// TOGGLE
+	const toggleShowMore = () => setShowMore(!showMore)
+
 	// HOOKS
 	const dogList = useDogsList()
 
@@ -19,18 +27,24 @@ const DogList: React.FC = () => {
 				<p>Click para ver</p>
 			</div>
 			<ul>
-				{dogList?.map((dogName, index) => (
-					<li key={dogName}>
-						<Link to={`/family/${dogName}`}>
-							<span>
-								<b>{index + 1}</b> {dogName.charAt(0).toUpperCase()}
-								{dogName.substring(1)}
-							</span>
-							<Icon name="arrow" height={20} width={20} />
-						</Link>
-					</li>
-				))}
+				{dogList?.map((dogName, index) =>
+					(!showMore && index < 3) || showMore ? (
+						<li key={dogName}>
+							<Link to={`/family/${dogName}`}>
+								<span>
+									<b>{index + 1}</b> {dogName.charAt(0).toUpperCase()}
+									{dogName.substring(1)}
+								</span>
+								<Icon name="arrow" height={20} width={20} />
+							</Link>
+						</li>
+					) : null,
+				)}
 			</ul>
+			<button onClick={toggleShowMore}>
+				<Icon name="down" height={20} width={20} />
+				{showMore ? 'Ver menos' : 'Ver más'}
+			</button>
 		</div>
 	)
 }
