68a71f40c0e7a33fe66bf531f70daf118b55ec2e
Alex Santos <alexdsantosv@gmail.com>
2022-05-20 01:17:34 -0600

    [ Add ] home title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,26 @@
 // REACT
 import React from 'react'
 
+// ASSETS
+import Doggo from 'assets/dog.png'
+
+// STYLES
+import Styles from './style.module.scss'
+
 const HomePage: React.FC = () => {
-	return <div>HomePage</div>
+	return (
+		<div className={Styles.container}>
+			<div className={Styles.titleContainer}>
+				<img src={Doggo} alt="Doggo" />
+				<div className={Styles.title}>
+					<h1>DogWorld</h1>
+					<p>
+						Si eres un <span>#DogLover</span> te encantara esta app.
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HomePage
